23481f8bfb230549e187c928bd4b969957e8f934
Victor Amaral de Sousa <victor.amaral.ds@gmail.com>
2021-01-26 08:07:46 +0100

    fix issue with participants recognition

--- src/main/kotlin/be/unamur/extension/MetaObjectExtension.kt
+++ src/main/kotlin/be/unamur/extension/MetaObjectExtension.kt
@@ -5,6 +5,6 @@ import be.unamur.metamodel.extension.MerodeExtension
 
 object MetaObjectExtension {
     fun isParticipant(metaObject: Metaobject, extension: MerodeExtension): Boolean {
-        return extension.participantTypes.participantType.contains(metaObject.id)
+        return extension.participantTypes.participantType.associateBy { it.objectTypeId }.contains(metaObject.id)
     }
 }
